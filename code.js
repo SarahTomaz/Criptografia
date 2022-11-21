9c92abf823ff23ffc7f0aca744c117a08256bac4
Valentina Lacerda <valentinaslacerda07@gmail.com>
2022-11-20 23:30:08 -0300

    novas funções

---
+++ code.js
@@ -0,0 +1,40 @@
+var cod = [
+  ("a", "A"),
+  ("b", "B"),
+  ("c", "C"),
+  ("d", "D"),
+  ("e", "E"),
+  ("f", "F"),
+  ("g", "G"),
+  ("h", "H"),
+  ("i", "I"),
+  ("j", "J"),
+  ("k", "K"),
+  ("l", "L"),
+  ("m", "M"),
+  ("n", "N"),
+  ("o", "O"),
+  ("p", "P"),
+  ("q", "Q"),
+  ("r", "R"),
+  ("s", "S"),
+  ("t", "T"),
+  ("u", "U"),
+  ("v", "V"),
+  ("w", "W"),
+  ("x", "X"),
+  ("y", "Y"),
+  ("z", "Z"),
+];
+
+document.getElementById("cripto").addEventListener("click", Criptografar);
+document.getElementById("descripto").addEventListener("click", Descriptografar);
+
+var userWords = document.getElementById("palavra").value;
+
+function Criptografar() {
+  while (userWords.length > 0) {}
+}
+
+function Descriptografar() {}
+//2*x + 3
