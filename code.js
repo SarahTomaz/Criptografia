2fd8d4021503d93de2cbcb57809bac40cf1aa21c
Valentina Lacerda <valentinaslacerda07@gmail.com>
2022-11-21 18:13:34 -0300

    alterações Criptografar

--- code.js
+++ code.js
@@ -1,40 +1,56 @@
 var cod = [
-  ("a", "A"),
-  ("b", "B"),
-  ("c", "C"),
-  ("d", "D"),
-  ("e", "E"),
-  ("f", "F"),
-  ("g", "G"),
-  ("h", "H"),
-  ("i", "I"),
-  ("j", "J"),
-  ("k", "K"),
-  ("l", "L"),
-  ("m", "M"),
-  ("n", "N"),
-  ("o", "O"),
-  ("p", "P"),
-  ("q", "Q"),
-  ("r", "R"),
-  ("s", "S"),
-  ("t", "T"),
-  ("u", "U"),
-  ("v", "V"),
-  ("w", "W"),
-  ("x", "X"),
-  ("y", "Y"),
-  ("z", "Z"),
+  ["1", "A"],
+  ["2", "B"],
+  ["3", "C"],
+  ["4", "D"],
+  ["5", "E"],
+  ["6", "F"],
+  ["7", "G"],
+  ["8", "H"],
+  ["9", "I"],
+  ["10", "J"],
+  ["11", "K"],
+  ["12", "L"],
+  ["13", "M"],
+  ["14", "N"],
+  ["15", "O"],
+  ["16", "P"],
+  ["17", "Q"],
+  ["18", "R"],
+  ["19", "S"],
+  ["20", "T"],
+  ["21", "U"],
+  ["22", "V"],
+  ["23", "W"],
+  ["24", "X"],
+  ["25", "Y"],
+  ["26", "Z"],
+  ["27", "."],
+  ["28", ","],
+  ["29", " "],
 ];
-
+var criptoList = [];
 document.getElementById("cripto").addEventListener("click", Criptografar);
 document.getElementById("descripto").addEventListener("click", Descriptografar);
 
-var userWords = document.getElementById("palavra").value;
-
 function Criptografar() {
-  while (userWords.length > 0) {}
+  var userWords = document.getElementById("palavra").value;
+
+  for (i = 0; i < userWords.length; i++) {
+    var c = 0;
+    while (c < 29) {
+      if (userWords[i] == cod[c][1]) {
+        const number = Number(cod[c][0]);
+        var result = 3 * number + 5;
+        if (result <= 29) {
+          criptoList.push(result);
+        }
+      }
+      c++;
+    }
+  }
+  console.log(criptoList);
 }
 
 function Descriptografar() {}
-//2*x + 3
+//3*x + 5
